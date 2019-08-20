4b3b776adefb3ddc6c39c97b91b92ee107bd1d97
heropoo <aiyouyou1000@163.com>
2019-08-20 10:45:51 +0800

    bower->npm

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Yii2-app-demo
 
 * mdmsoft/yii2-admin
+* [更换bower包为npm包，加速composer下载](https://www.yiichina.com/tutorial/1676)
--- common/config/main.php
+++ common/config/main.php
@@ -1,8 +1,11 @@
 <?php
 return [
     'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        //'@bower' => '@vendor/bower-asset',
+        //'@npm'   => '@vendor/npm-asset',
+        //
+        '@bower' => dirname(dirname(__DIR__)) . '/node_modules',
+        '@npm' => dirname(dirname(__DIR__)) . '/node_modules',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
